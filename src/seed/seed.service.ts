769b2651982c85d010a1eb0bbdfe2d0833d3c759
Matías Lobo <matiasnl.1993@gmail.com>
2024-01-22 01:04:59 -0300

    Popular la base de datos con el SEED de productos

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/product-data';
 
 @Injectable()
 export class SeedService {
@@ -12,5 +13,11 @@ export class SeedService {
 
   private async insertNewProducts() {
     await this.productsService.deleteAllProducts();
+
+    const products = initialData.products;
+
+    const insertPromises = products.map((p) => this.productsService.create(p));
+
+    await Promise.all(insertPromises);
   }
 }
