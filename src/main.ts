5dcdda916f74555b5e93a0f0153e7dee84391bb1
Matías Lobo <matiasnl.1993@gmail.com>
2024-01-27 02:12:13 -0300

    Instalación de módulo de OpenAPI para documentar

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Teslo App")
+    .setDescription("A simple API made with NestJS")
+    .setVersion("1.0")
+    .build()
+
   const logger = new Logger('bootstrap');
   app.setGlobalPrefix('/api');
   app.useGlobalPipes(
@@ -12,6 +20,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
   logger.log(`App running on port ${process.env.PORT}`);
 }
