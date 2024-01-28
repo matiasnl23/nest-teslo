0a6e6e7ff3fb48d4616f8b65f68f7a51a4f83d64
Matías Lobo <matiasnl.1993@gmail.com>
2024-01-27 23:46:49 -0300

    Instalando socket.io y implementar listener de conexión/desconexión

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,24 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Logger } from '@nestjs/common';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect {
+  logger: Logger = new Logger(MessagesWsGateway.name);
+
+  constructor(private readonly messagesWsService: MessagesWsService) { }
+
+  handleConnection(client: Socket) {
+    this.logger.debug(`Cliente conectado: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.debug(`Cliente desconectado: ${client.id}`);
+  }
 }
